057e1ce404c2fbc260fac7ed0382d9c818d06330
Rapheal Odejinmi <odejinmirapheal@gmail.com>
2024-02-11 19:05:38 +0100

    category page added

--- utils/getData.ts
+++ utils/getData.ts
@@ -3,8 +3,8 @@ import { cache } from "react";
 async function fetchData() {
   try {
     const res = await fetch(
-      // "http://139.59.145.27:3000/blog_category/post"
-      "https://api.jsonsilo.com/public/ef9df789-0db9-4402-8bd1-d33254214790"
+      "http://139.59.145.27:3000/blog_category/post"
+      // "https://api.jsonsilo.com/public/ef9df789-0db9-4402-8bd1-d33254214790"
     );
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
