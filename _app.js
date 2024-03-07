6c2165c2807c9868b5a8c794c232c25d9e46e803
Odejinmi Tobiloba <odetobi123@gmail.com>
2024-03-07 10:12:16 +0100

    some ads

--- _app.js
+++ _app.js
@@ -1,6 +1,6 @@
-<Script
+{/* <Script
   id="adsbygoogle-init"
   strategy="afterInteractive"
   crossOrigin="anonymous"
   src= "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1435412513227114"
-/>
+/> */}
